e7d22059fe8c418ca40da18950af7852c45cd901
Patheit <andrewtnguyen0106@gmail.com>
2026-02-13 23:29:56 -0800

    updated tabbing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,11 +3,11 @@ vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 
--- vim.opt.softtabstop = 4
-vim.opt.tabstop = 4
-vim.opt.smartindent = true
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.smartindent = true -- Make indenting "smart" (e.g., adding a level after '{')
 
 vim.opt.clipboard = "unnamedplus"
 
